4d6d1ffe76495352470f462c6843f4ee06bea055
Nabil Noh <19523841+mrnabilnoh@users.noreply.github.com>
2022-05-18 23:04:45 +0800

    Test: add cloudflare kv data store connection

--- functions/api/todos/index.ts
+++ functions/api/todos/index.ts
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid'
 
 
-// Default TODO item
+// default value for TODO item
 const defaultData : TODO = {
   id: 1,
   name: 'Sample Todo items.',
@@ -9,26 +9,26 @@ const defaultData : TODO = {
 };
 
 /**
- * @param  {} {request
- * @param  {} env}
+ * @param  {} request: Http Request
+ * @param  {} env: Cloudflare Environment
  * @returns Promise
  */
 export const onRequestGet: PagesFunction<ENV> = async ({request, env}) : Promise<Response> =>  {
-    /**
-     * Get Client IP from Cloudflare meta header.
-     * This will help to split user by ip session.
-     * @param  {} 'CF-Connecting-IP'
-     */
+    // get client IP from Cloudflare meta header.
     const ip : string = request.headers.get('CF-Connecting-IP');
     const storeKey : string = `store-${ip}`;
   
-    // get current ip session todo list link (if exist) in Cloudflare KV data store.
+    // get current todo list link (if exist) based on 'ip session' in Cloudflare KV data store.
     let data = await env.TODOS_STORAGE.get(storeKey, { type: "json"}) as string[];
+    /**
+     * if data equal to 'null', then we assume this 'ip' is a new session.
+     * proceed to create sample todo list item for example.
+     */
     if (data == null) {
         // generate new unique id for sample todo list link
         const uniqueId = nanoid(6);
         data = [uniqueId];
-        // store new todo link in Cloudflare KV data store
+        // store new todo link short link in Cloudflare KV data store
         await env.TODOS_STORAGE.put(storeKey, JSON.stringify(data));
         // store new todo item in Cloudflare KV data store
         await env.TODOS_STORAGE.put(uniqueId, JSON.stringify([defaultData]), { metadata:  {
@@ -36,9 +36,12 @@ export const onRequestGet: PagesFunction<ENV> = async ({request, env}) : Promise
         } as TODO_META });
     }
 
-    let items = [] as TODO_LIST[];
+    let items = [];
     for (const key in data) {
+      items.push(key)
       const {value, metadata} = await env.TODOS_STORAGE.getWithMetadata<TODO[],TODO_META>(key, { type: "json"});
+      items.push(value)
+      items.push(metadata)
       if (value != null) {
         items.push({
           text: metadata.text,
