85d31c26505a7e922db1aefbe1b126d0bf2b809f
Nabil Noh <mrnabilnoh@icloud.com>
2022-05-18 08:27:49 +0800

    Test: add cloudflare kv data store connection

--- functions/api/todos/index.ts
+++ functions/api/todos/index.ts
@@ -2,7 +2,6 @@
 export const onRequestGet: PagesFunction<Envtest> = async ({env}) : Promise<Response> =>  {
    // Contents of context object
    const value = await env.TODOS_STORAGE.get("dedSYekRZGE");
-   const value2 = await TODOS_STORAGE.get("dedSYekRZGE");
 
-   return new Response(value + " : " + value2);
+   return new Response(value);
 }
