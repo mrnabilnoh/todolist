3ac559941c1168f84579d7307bb34232e6dd440d
Nabil Noh <19523841+mrnabilnoh@users.noreply.github.com>
2022-05-19 18:11:57 +0800

    Test: cloudflare function api/todos/[id]

--- functions/api/todos/[id].ts
+++ functions/api/todos/[id].ts
@@ -7,8 +7,15 @@ export const onRequest: TodoPagesFunction = async () => {
 export const onRequestGet: TodoPagesFunction = async ({ request, env, params }) => {
   const clientIp: string = request.headers.get("CF-Connecting-IP");
 
+  // handle invalid id
+  if(params.id == null || params.id.length < 10) {
+    return new Response(JSON.stringify({
+      message: "404 Not Found"
+    }), { status : 404});
+  }
+
   return new Response(JSON.stringify({
       clientIp: clientIp,
-      param: params
+      param: params.id
   }));
 };
