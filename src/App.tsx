a395643fb07c02de175338413383a274b19e39ca
Nabil Noh <19523841+mrnabilnoh@users.noreply.github.com>
2022-05-20 08:50:55 +0800

    Test: todo template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="todo-app">
-      <TodoList/>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function PageNotFound() {
+  return (
+    <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">
+        404
+      </h1>
+      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+        Page Not Found
+      </div>
+      <div className="mt-5">
+        <button className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+          <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+            <Link to="/">Go Home</Link>
+          </span>
+        </button>
+      </div>
+    </main>
   );
 }
 
